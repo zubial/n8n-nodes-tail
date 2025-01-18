b6699d83e6831859640062b2fa20171f6cb07297
brodrigues <rodrigues.bertrand@gmail.com>
2025-01-18 12:06:45 +0100

    Refactor Tail Trigger node and update metadata
    Streamline Tail Trigger configuration by consolidating "Last Lines" into an options collection and enhancing error handling. Updated project metadata in `package.json` and renamed configuration files for better consistency.

--- nodes/Tail/Tail.node.ts
+++ nodes/Tail/Tail.node.ts
@@ -1,83 +1,105 @@
-import { INodeType, INodeTypeDescription, ITriggerFunctions, ITriggerResponse } from 'n8n-workflow';
-import { ChildProcessWithoutNullStreams, spawn } from 'child_process';
+import {
+    IDataObject,
+    INodeType,
+    INodeTypeDescription,
+    ITriggerFunctions,
+    ITriggerResponse,
+} from 'n8n-workflow';
+import {ChildProcessWithoutNullStreams, spawn} from 'child_process';
 
 export class Tail implements INodeType {
-	description: INodeTypeDescription = {
-		displayName: 'Tail Trigger',
-		name: 'tail',
-		icon: 'fa:terminal',
-		group: ['trigger'],
-		version: 1,
-		subtitle: '={{$parameter["file"]}}',
-		description: 'Starts the workflow when lines are added to this file',
-		defaults: {
-			name: 'Tail Trigger',
-		},
-		inputs: ['main'],
-		outputs: ['main'],
-		properties: [
-			{
-				displayName: 'Directory',
-				name: 'directory',
-				type: 'string',
-				default: '/home/user/',
-				required: true,
-				description: 'Define a working directory',
-			},
-			{
-				displayName: 'File',
-				name: 'file',
-				type: 'string',
-				default: 'file.log',
-				required: true,
-				description: 'Specify a file to monitor for real-time updates',
-			},
-			{
-				displayName: 'Last Lines',
-				name: 'last',
-				type: 'number',
-				default: 0,
-				required: true,
-				description: 'How many last lines are loaded on startup?',
-			},
-		],
-	};
+    description: INodeTypeDescription = {
+        displayName: 'Tail Trigger',
+        name: 'tail',
+        icon: 'fa:terminal',
+        group: ['trigger'],
+        version: 1,
+        triggerPanel: true,
+        subtitle: '={{$parameter["file"]}}',
+        description: 'Starts the workflow when lines are added to this file',
+        defaults: {
+            name: 'Tail Trigger',
+        },
+        inputs: ['main'],
+        outputs: ['main'],
+        properties: [
+            {
+                displayName: 'Directory',
+                name: 'directory',
+                type: 'string',
+                default: '/home/user/',
+                required: true,
+                description: 'Define a working directory',
+            },
+            {
+                displayName: 'File',
+                name: 'file',
+                type: 'string',
+                default: 'file.log',
+                required: true,
+                description: 'Specify a file to monitor for real-time updates',
+            },
+            {
+                displayName: 'Options',
+                name: 'options',
+                type: 'collection',
+                placeholder: 'Add option',
+                default: {},
+                options: [
+                    {
+                        displayName: 'Last Lines',
+                        name: 'last',
+                        type: 'number',
+                        default: 0,
+                        description: 'How many last lines are loaded on startup?',
+                    },
+                ],
+            },
+        ],
+    };
 
-	async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
-		const directory = this.getNodeParameter('directory') as string;
-		const file = this.getNodeParameter('file') as string;
-		const last = this.getNodeParameter('last') as number;
+    async trigger(this: ITriggerFunctions): Promise<ITriggerResponse> {
+        const directory = this.getNodeParameter('directory') as string;
+        const file = this.getNodeParameter('file') as string;
 
-		const args: string[] = ['-f', '-n', last.toString(), directory + file];
-		console.log(args);
+        const options = this.getNodeParameter('options') as IDataObject;
+        const last = options.last || 0;
 
-		let child: ChildProcessWithoutNullStreams = spawn('tail', args);
-		child.stderr.pipe(process.stderr); // Redirect stderr to the console for error output.
+        const args: string[] = ['-f', '-n', last.toString(), directory + file];
 
-		child.stdout.on('data', (data: Buffer) => {
-			console.log(data.toString());
+        console.log(`Tail process starting on ${directory}${file}`);
 
-			const lines: string[] = data
-				.toString()
-				.split('\n')
-				.filter((line) => line.trim() !== ''); // Split lines and filter out empty lines
+        let child: ChildProcessWithoutNullStreams = spawn('tail', args);
+        child.stderr.pipe(process.stderr); // Redirect stderr to the console for error output.
 
-			for (const line of lines) {
-				this.emit([this.helpers.returnJsonArray([{ line: line }])]);
-			}
-		});
+        child.stdout.on('data', (data: Buffer) => {
+            console.log(data.toString());
 
-		child.stderr.on('data', (error: Buffer) => {
-			child.kill();
-			this.emit([this.helpers.returnJsonArray([{ error: error.toString() }])]);
-		});
+            const lines: string[] = data
+                .toString()
+                .split('\n')
+                .filter((line) => line.trim() !== ''); // Split lines and filter out empty lines
 
-		async function closeFunction() {
-			child.kill();
-		}
+            for (const line of lines) {
+                this.emit([this.helpers.returnJsonArray([{line: line}])]);
+            }
+        });
 
-		return {
-			closeFunction,
-		};
-	}
+        child.on('close', (code, signal) => {
+            console.log(`Tail process terminated on ${directory}${file}`);
+        });
+
+        child.stderr.on('data', (error: Buffer) => {
+            closeFunction();
+            this.emitError(new Error(error.toString()));
+        });
+
+        async function closeFunction() {
+            child.kill('SIGHUP');
+        }
+
+        return {
+            closeFunction,
+        };
+    }
 }
